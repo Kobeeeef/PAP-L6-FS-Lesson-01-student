a26741e8d2f82da6fa2ba652636eb1fcc986fa82
kobel <kobelei335@gmail.com>
2025-01-20 20:59:40 -0800

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const { connectMongoose } = require('./connect');
+const Message = require('./models/Message');
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -9,65 +10,51 @@ app.use(cors());
 app.use(express.json());
 
 // Home dynamic route to get public or secret messages
-const read = require('./models/read-messages');
 app.get('/:secret', async (req, res) => {
-
-    const results = await read.getAllMessages(req.params.secret);
+    const results = await Message.readAll(req.params.secret);
     res.send(results);
 
     console.log("GET request received on home page");
 });
 
-
 // Post route to post a new message
-const creator = require('./models/create-message');
 app.post('/message', async (req, res) => {
-
     const newMessage = req.body;
-    const results = await creator.createNewMessage(newMessage);
+    const results = await Message.createNew(newMessage);
     res.sendStatus(201);
 
-    console.log("POST request received on message route")
+    console.log("POST request received on message route");
     console.log(`New message created with id: ${results._id}`);
 });
 
-
 // Update route to update an existing message
-const updater = require('./models/update-message');
 app.patch('/message/:id', async (req, res) => {
-
     const messageUpdate = req.body;
-    const results = await updater.updateMessage(req.params.id, messageUpdate);
+    await Message.update(req.params.id, messageUpdate);
     res.sendStatus(200);
 
-    console.log("PATCH request received on message route")
+    console.log("PATCH request received on message route");
     console.log(`Message with id ${req.params.id} updated`);
 });
 
-
 // Delete route to delete an existing message
-const deleter = require('./models/delete-message');
 app.delete('/message/:id', async (req, res) => {
-
-    const results = await deleter.deleteMessage(req.params.id);
+    await Message.delete(req.params.id);
     res.sendStatus(200);
 
-    console.log("DELETE request received on message route")
+    console.log("DELETE request received on message route");
     console.log(`Message with id ${req.params.id} deleted`);
 });
-
-
 
 //* ********************* Launching the server **************** */
 
 const start = async () => {
     try {
-        await connectMongoose(); //Exercise 2.7 run the new connect function
+        await connectMongoose();
         app.listen(port, () => console.log(`Server running on port ${port}...`));
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
     }
-}
+};
 
 start();
--- models/Message.js
+++ models/Message.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
+const { Schema, model } = require('mongoose');
+const connectMongoose = require('./connect.js');
 
 const collectionName = process.env.COLLECTION_NAME;
-const { Schema, model } = require('mongoose');
 
 const messageSchema = new Schema({
     message: String,
@@ -9,6 +10,59 @@ const messageSchema = new Schema({
     date: Date,
     secret: Boolean
 });
+
+class MessageClass {
+    static async readAll(isSecret) {
+        try {
+            if (isSecret === "true" || isSecret === true) {
+                isSecret = true;
+            } else if (isSecret === "false" || isSecret === false) {
+                isSecret = false;
+            }
+            return await this.find({ secret: isSecret }).sort({ date: -1 }).exec();
+        } catch (err) {
+            console.error(err);
+            return [];
+        }
+    }
+
+    static async createNew(newMessage) {
+        try {
+            const result = await this.create(newMessage);
+            console.log("Message inserted: ", result);
+            return result;
+        } catch (err) {
+            console.error(err);
+            return { _id: -1 };
+        }
+    }
+
+    static async update(messageId, messageUpdate) {
+        try {
+            const result = await this.updateOne({ _id: messageId }, messageUpdate);
+            console.log("Message updated: ", result);
+            return result;
+        } catch (err) {
+            console.error(err);
+            return {
+                modifiedCount: 0,
+                acknowledged: false
+            };
+        }
+    }
+    static async delete(messageId) {
+        try {
+            const result = await this.deleteOne({ _id: messageId });
+            console.log("Message deleted: ", result);
+            return result;
+        } catch (err) {
+            console.error(err);
+            return { deletedCount: 0 };
+        }
+    }
+}
+
+messageSchema.loadClass(MessageClass);
 
 const Message = model('Message', messageSchema, collectionName);
 
